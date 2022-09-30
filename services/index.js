767802514971a057eee283476c422806b5411efb
muqeetmughal <muqeetmughal786@gmail.com>
2022-10-01 01:27:13 +0500

    blog initiated

--- services/index.js
+++ services/index.js
@@ -1,1 +1,12 @@
-import {request, gql} from "graphql-request";
+// import {request, gql} from "graphql-request";
+
+// ./apollo-client.js
+
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "https://api-us-east-1.hygraph.com/v2/cl85fvr1i0xk401t180um6lid/master",
+    cache: new InMemoryCache(),
+});
+
+export default client;
